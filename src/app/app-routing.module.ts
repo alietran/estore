3c76157705b4300871fd3146bf2e85665d7dd2a8
dbs-ngocdiep <diep@deltabrains.vn>
2024-06-12 15:58:51 +0700

    test commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,27 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { AdminDashboardComponent } from "./pages/admin-dashboard/admin-dashboard.component";
-import { AdminLayoutComponent } from "./pages/admin-layout/admin-layout.component";
-import { NotFoundComponent } from "./pages/not-found/not-found.component";
-import { ProductsComponent } from "./pages/products/products.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { AdminLayoutComponent } from './pages/admin-layout/admin-layout.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 const routes: Routes = [
   {
-    path: "home",
+    path: 'home',
     loadChildren: () =>
-      import("./pages/home/home.module").then((m) => m.HomeModule),
+      import('./pages/home/home.module').then((m) => m.HomeModule),
   },
-  { path: "", redirectTo: "home/products", pathMatch: "full" },
-
+  { path: '', redirectTo: 'home/products', pathMatch: 'full' },
   {
-    path: "admin",
+    path: 'admin',
     component: AdminLayoutComponent,
     children: [
-      { path: "dashboard", component: AdminDashboardComponent },
-      { path: "products", component: ProductsComponent },
+      { path: 'dashboard', component: AdminDashboardComponent },
+      { path: 'products', component: ProductsComponent },
     ],
   },
   {
-    path: "**",
+    path: '**',
     component: NotFoundComponent,
   },
 ];
